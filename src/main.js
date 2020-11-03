34c8f7cc68554b9a00140b2dcfccf214aae6f53b
caoshenyang <2417254000@qq.com>
2020-11-03 10:12:09 +0800

    基于element-admin-templete修改

--- src/main.js
+++ src/main.js
@@ -1,17 +1,43 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
 import router from './router'
-import './plugins/element.js'
-// 导入字体图标样式库
-import './assets/fonts/iconfont.css'
-import './assets/css/gloabal.css'
-import axios from 'axios'
+
+import '@/icons' // icon
+import '@/permission' // permission control
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+if (process.env.NODE_ENV === 'production') {
+  const { mockXHR } = require('../mock')
+  mockXHR()
+}
+
+// set ElementUI lang to EN
+Vue.use(ElementUI, { locale })
+// 如果想要中文版 element-ui，按如下方式声明
+// Vue.use(ElementUI)
 
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-Vue.prototype.$http = axios
 
 new Vue({
+  el: '#app',
   router,
+  store,
   render: h => h(App)
-}).$mount('#app')
+})
